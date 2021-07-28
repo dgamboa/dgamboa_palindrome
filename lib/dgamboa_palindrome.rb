5405522f391172993fa62bbad17cc2e96466500b
Daniel Gamboa <danielgamboa10@gmail.com>
2021-07-28 15:49:22 -0500

    Initialize repo

---
+++ lib/dgamboa_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "dgamboa_palindrome/version"
+
+module DgamboaPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/dgamboa_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module DgamboaPalindrome
+  VERSION = "0.1.0"
+end
