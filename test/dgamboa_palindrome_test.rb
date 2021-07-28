c8c1cfde02d588b0adee233490c525359bf6b592
Daniel Gamboa <danielgamboa10@gmail.com>
2021-07-28 16:15:17 -0500

    Add four palindrome tests

--- test/dgamboa_palindrome_test.rb
+++ test/dgamboa_palindrome_test.rb
@@ -3,11 +3,20 @@
 require "test_helper"
 
 class DgamboaPalindromeTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::DgamboaPalindrome::VERSION
+
+  def test_non_palindrome
+    assert !"apple".palindrome?
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_literal_palindrome
+    assert "racecar".palindrome?
+  end
+
+  def test_mixed_case_palindrome
+    skip
+  end
+
+  def test_palindrome_with_punctuation
+    skip
   end
 end
